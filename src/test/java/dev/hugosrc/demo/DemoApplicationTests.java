5fd27f685e9b7f846b4d8a4286436ff8e0331529
hugosrc <hugosr.contato@gmail.com>
2021-10-23 21:54:52 -0300

    setup spring boot project

---
+++ src/test/java/dev/hugosrc/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.hugosrc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
